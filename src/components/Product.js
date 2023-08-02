e66a1d07b404a4592845a393201ddbfe00c76ff4
khadijahnabil <nabilkhadijah7@gmail.com>
2023-08-02 22:37:39 +0200

    formatted price from cents to dollars

--- src/components/Product.js
+++ src/components/Product.js
@@ -15,7 +15,7 @@ const Product = ({ id, image, name, price }) => {
       </div>
       <footer>
         <h5>{name}</h5>
-        <p>${price}</p>
+        <p>{formatPrice(price)}</p>
       </footer>
     </Wrapper>
   );
