0625d8e50b1ceb50ff463c3c9cf6257238ccadb9
khadijahnabil <nabilkhadijah7@gmail.com>
2023-08-02 22:46:03 +0200

    fixed single product link

--- src/App.js
+++ src/App.js
@@ -35,7 +35,7 @@ function App() {
         <Route exact path='/products'>
           <Products />
         </Route>
-        <Route exact path='products/:id' children={<SingleProduct />} />
+        <Route exact path='/products/:id' children={<SingleProduct />} />
         <Route exact path='/checkout'>
           <Checkout />
         </Route>
