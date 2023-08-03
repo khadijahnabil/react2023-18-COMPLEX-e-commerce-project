1f155c1bb761f6f50f12c265cb9e0a33e6c2e38d
khadijahnabil <nabilkhadijah7@gmail.com>
2023-08-03 20:55:52 +0200

    set ProductImages component in SingleProductPage

--- src/components/ProductImages.js
+++ src/components/ProductImages.js
@@ -1,9 +1,26 @@
-import React, { useState } from 'react'
-import styled from 'styled-components'
+import React, { useState } from "react";
+import styled from "styled-components";
 
-const ProductImages = () => {
-  return <h4>product images</h4>
-}
+const ProductImages = ({ images = [{ url: "", filename: "" }] }) => {
+  const [main, setMain] = useState(images[0]);
+  return (
+    <Wrapper>
+      <img src={main.url} alt={main.filename} className='main' />
+      <div className='gallery'>
+        {images.map((image, index) => {
+          return (
+            <img
+              src={image.url}
+              alt={image.filename}
+              key={index}
+              onClick={() => setMain(images[index])}
+              className={image.url === main.url ? "active" : null}></img>
+          );
+        })}
+      </div>
+    </Wrapper>
+  );
+};
 
 const Wrapper = styled.section`
   .main {
@@ -48,6 +65,6 @@ const Wrapper = styled.section`
       }
     }
   }
-`
+`;
 
-export default ProductImages
+export default ProductImages;
