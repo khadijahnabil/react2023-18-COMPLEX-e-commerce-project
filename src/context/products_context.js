be821b848afe9dd82e1987366a56a72096cc9070
khadijahnabil <nabilkhadijah7@gmail.com>
2023-08-03 08:19:08 +0200

    setup single product fetching & reducer

--- src/context/products_context.js
+++ src/context/products_context.js
@@ -16,6 +16,9 @@ const initialState = {
   products_error: false,
   products: [],
   featured_products: [],
+  single_product_loading: false,
+  single_product_error: false,
+  single_product: {},
 };
 
 const ProductsContext = React.createContext();
@@ -37,6 +40,17 @@ export const ProductsProvider = ({ children }) => {
   useEffect(() => {
     fetchProducts(url);
   }, []);
+
+  const fetchSingleProduct = async (url) => {
+    dispatch({ type: GET_SINGLE_PRODUCT_BEGIN });
+    try {
+      const response = await axios.get(url);
+      const singleProduct = response.data;
+      dispatch({ type: GET_SINGLE_PRODUCT_SUCCESS, payload: singleProduct });
+    } catch (error) {
+      dispatch({ type: GET_SINGLE_PRODUCT_ERROR });
+    }
+  };
 
   return (
     <ProductsContext.Provider value={{ ...state, fetchProducts }}>
