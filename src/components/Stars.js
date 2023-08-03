58c2237809adde802d2903b9866c0274158941d9
khadijahnabil <nabilkhadijah7@gmail.com>
2023-08-03 21:23:40 +0200

    finalized Stars component for SingleProductPage array approach

--- src/components/Stars.js
+++ src/components/Stars.js
@@ -2,18 +2,24 @@ import React from "react";
 import styled from "styled-components";
 import { BsStarFill, BsStarHalf, BsStar } from "react-icons/bs";
 const Stars = ({ stars, reviews }) => {
-  console.log(stars);
-  return (
-    <Wrapper>
-      <span>
-        {stars >= 1 ? (
+  const tempStars = Array.from({ length: 5 }, (_, index) => {
+    const number = index + 0.5;
+
+    return (
+      <span key={index}>
+        {stars >= index + 1 ? (
           <BsStarFill />
-        ) : stars >= 0.5 ? (
+        ) : stars >= number ? (
           <BsStarHalf />
         ) : (
           <BsStar />
         )}
       </span>
+    );
+  });
+  return (
+    <Wrapper>
+      <div className='stars'>{tempStars}</div>
       <p className='reviews'>({reviews} customer reviews)</p>
     </Wrapper>
   );
