42f70797ccac30a4e61fdccc35d977a6e64fea08
khadijahnabil <nabilkhadijah7@gmail.com>
2023-08-04 16:58:09 +0200

    set up ListView component for ProductsPage

--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -4,8 +4,18 @@ import GridView from "./GridView";
 import ListView from "./ListView";
 
 const ProductList = () => {
-  const { filtered_products: products } = useFilterContext();
-  return <GridView products={products}>product list</GridView>;
+  const { filtered_products: products, grid_view } = useFilterContext();
+  if (products.length < 1) {
+    return (
+      <h5 style={{ textTransform: "none" }}>
+        Sorry, there are no matching products...
+      </h5>
+    );
+  }
+  if (grid_view === false) {
+    return <ListView products={products} />;
+  }
+  return <GridView products={products} />;
 };
 
 export default ProductList;
