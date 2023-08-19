cb2377a05ce8d0d4d04d4b90547711d1091c050b
khadijahnabil <nabilkhadijah7@gmail.com>
2023-08-19 18:21:37 +0200

    set up filters default value

--- src/reducers/filter_reducer.js
+++ src/reducers/filter_reducer.js
@@ -11,10 +11,18 @@ import {
 
 const filter_reducer = (state, action) => {
   if (action.type === LOAD_PRODUCTS) {
+    let maxPrice = action.payload.map((product) => product.price);
+    maxPrice = Math.max(...maxPrice);
+
     return {
       ...state,
       all_products: [...action.payload],
       filtered_products: [...action.payload],
+      filters: {
+        ...state.filters,
+        max_price: maxPrice,
+        price: maxPrice,
+      },
     };
   }
 
