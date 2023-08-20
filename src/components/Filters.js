4a0b2f9c56b52395e9b4288981b2a07ba7cf12ef
khadijahnabil <nabilkhadijah7@gmail.com>
2023-08-20 14:53:14 +0200

    set up filters for companies unique values

--- src/components/Filters.js
+++ src/components/Filters.js
@@ -56,6 +56,22 @@ const Filters = () => {
               })}
             </div>
           </div>
+          <div className='form-control'>
+            <h5>company</h5>
+            <select
+              name='company'
+              value={company}
+              onChange={updateFilters}
+              className='company'>
+              {companies.map((c, index) => {
+                return (
+                  <option value={c} key={index}>
+                    {c}
+                  </option>
+                );
+              })}
+            </select>
+          </div>
         </form>
       </div>
     </Wrapper>
