d4a286bea1642617521ce089b763d62ad5551bba
khadijahnabil <nabilkhadijah7@gmail.com>
2023-08-22 23:40:51 +0200

    set up ADD_TO_CART for existing items

--- src/reducers/cart_reducer.js
+++ src/reducers/cart_reducer.js
@@ -9,9 +9,30 @@ import {
 const cart_reducer = (state, action) => {
   if (action.type === ADD_TO_CART) {
     const { id, color, amount, product } = action.payload;
+    // 1 first, we find if the item is in the cart
     const tempItem = state.cart.find((item) => item.id === id + color);
+    // 3 if the item is already in the cart, then this happens
     if (tempItem) {
+      // 4 first, we iterate over the cart to check where item is
+      const tempCart = state.cart.map((cartItem) => {
+        //  5 when id matches the cart id, we know its the same item
+        if (cartItem.id === id + color) {
+          // 6 so we increase the amount
+          let newAmount = cartItem.amount + amount;
+          // 7 but make sure that its not more than the stock
+          if (newAmount > cartItem.max) {
+            newAmount = cartItem.max;
+          }
+          // 8 then we change the amount property to the new amount
+          return { ...state, amount: newAmount };
+        } else {
+          // 9 if cartItem id doesn't match, we just return to our array
+          return cartItem;
+        }
+      });
+      return { ...state, cart: tempCart };
     } else {
+      // 2 if item is not in the cart already, then this happens
       const newItem = {
         id: id + color,
         name: product.name,
