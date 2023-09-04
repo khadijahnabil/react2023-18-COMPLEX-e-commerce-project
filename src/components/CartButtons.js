94ffc9428eca869edd3076411e5c2a54e17efd28
khadijahnabil <nabilkhadijah7@gmail.com>
2023-09-04 14:22:22 +0200

    set up stripe checkout & set up netlify function

--- src/components/CartButtons.js
+++ src/components/CartButtons.js
@@ -8,7 +8,7 @@ import { useUserContext } from "../context/user_context";
 
 const CartButtons = () => {
   const { closeSidebar } = useSidebarContext();
-  const { total_items } = useCartContext();
+  const { total_items, clearCart } = useCartContext();
   const { loginWithRedirect, logout, myUser } = useUserContext();
   return (
     <Wrapper className='cart-btn-wrapper'>
@@ -23,7 +23,10 @@ const CartButtons = () => {
         <button
           type='button'
           className='auth-btn'
-          onClick={() => logout({ returnTo: window.location.origin })}>
+          onClick={() => {
+            clearCart();
+            logout({ returnTo: window.location.origin });
+          }}>
           Logout <FaUserMinus />
         </button>
       ) : (
